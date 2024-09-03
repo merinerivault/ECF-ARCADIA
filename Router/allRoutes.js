16d8e1d831c655ee28e29adfd2fdd0522dc8099a
merine <ambre92000@hotmail.com>
2024-09-03 23:07:00 +0200

    Section habitats dans accueil

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/accueil", "Accueil", "/pages/home.html"),
+    new Route("/services", "Services", "/pages/services.html"),
+    new Route("/habitats", "Habitats", "/pages/habitats.html"),
+    new Route("/jungle", "Jungle", "/pages/jungle.html"),
+    new Route("/savane", "Savane", "/pages/savane.html"),
+    new Route("/marais", "marais", "/pages/marais.html"),
+    new Route("/services", "services", "/pages/services.html"),
+];
+  
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
