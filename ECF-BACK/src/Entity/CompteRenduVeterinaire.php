d2714162c6e1bc16bc0bcecc21e2003cbb305362
merine <ambre92000@hotmail.com>
2024-11-12 15:43:01 +0100

    Mise à jour Compte rendu + ralation avec veterinaire

--- ECF-BACK/src/Entity/CompteRenduVeterinaire.php
+++ ECF-BACK/src/Entity/CompteRenduVeterinaire.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CompteRenduVeterinaireRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CompteRenduVeterinaireRepository::class)]
@@ -13,8 +14,54 @@ class CompteRenduVeterinaire
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $rapport = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\ManyToOne(inversedBy: 'compteRenduVeterinaires')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?veterinaire $veterinaire = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getRapport(): ?string
+    {
+        return $this->rapport;
+    }
+
+    public function setRapport(string $rapport): static
+    {
+        $this->rapport = $rapport;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getVeterinaire(): ?veterinaire
+    {
+        return $this->veterinaire;
+    }
+
+    public function setVeterinaire(?veterinaire $veterinaire): static
+    {
+        $this->veterinaire = $veterinaire;
+
+        return $this;
     }
 }
--- ECF-BACK/src/Entity/Veterinaire.php
+++ ECF-BACK/src/Entity/Veterinaire.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VeterinaireRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VeterinaireRepository::class)]
@@ -15,6 +17,17 @@ class Veterinaire
 
     #[ORM\Column(length: 255)]
     private ?string $specialite = null;
+
+    /**
+     * @var Collection<int, CompteRenduVeterinaire>
+     */
+    #[ORM\OneToMany(targetEntity: CompteRenduVeterinaire::class, mappedBy: 'veterinaire', orphanRemoval: true)]
+    private Collection $compteRenduVeterinaires;
+
+    public function __construct()
+    {
+        $this->compteRenduVeterinaires = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Veterinaire
     public function setSpecialite(string $specialite): static
     {
         $this->specialite = $specialite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CompteRenduVeterinaire>
+     */
+    public function getCompteRenduVeterinaires(): Collection
+    {
+        return $this->compteRenduVeterinaires;
+    }
+
+    public function addCompteRenduVeterinaire(CompteRenduVeterinaire $compteRenduVeterinaire): static
+    {
+        if (!$this->compteRenduVeterinaires->contains($compteRenduVeterinaire)) {
+            $this->compteRenduVeterinaires->add($compteRenduVeterinaire);
+            $compteRenduVeterinaire->setVeterinaire($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCompteRenduVeterinaire(CompteRenduVeterinaire $compteRenduVeterinaire): static
+    {
+        if ($this->compteRenduVeterinaires->removeElement($compteRenduVeterinaire)) {
+            // set the owning side to null (unless already changed)
+            if ($compteRenduVeterinaire->getVeterinaire() === $this) {
+                $compteRenduVeterinaire->setVeterinaire(null);
+            }
+        }
 
         return $this;
     }
