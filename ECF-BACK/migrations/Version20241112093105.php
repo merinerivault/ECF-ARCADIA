423ee943727446100f6bc7b93b988811c932d3f1
merine <ambre92000@hotmail.com>
2024-11-12 10:41:10 +0100

    Ajout des tables BDD

---
+++ ECF-BACK/migrations/Version20241112093105.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241112093105 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(100) NOT NULL, espece VARCHAR(255) NOT NULL, date_naissance DATETIME NOT NULL, description LONGTEXT NOT NULL, nourriture VARCHAR(255) NOT NULL, gramme_nourriture INT NOT NULL, date_passage DATETIME NOT NULL, id_image INT NOT NULL, id_habitat INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE animal');
+    }
+}
